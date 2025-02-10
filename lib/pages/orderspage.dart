087ace42cbf7c4f5a011d700d1199db922610649
Mattia Picchio <picchiomattia@gmail.com>
2025-02-10 13:39:07 +0100

    feat: refactor OrdersPage to use DataManager for cart management and improve order display

--- lib/pages/orderspage.dart
+++ lib/pages/orderspage.dart
@@ -1,10 +1,125 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_coffee/datamanager.dart';
+import 'package:flutter_coffee/datamodel.dart';
 
-class OrdersPage extends StatelessWidget {
-  const OrdersPage({super.key});
+class OrderPage extends StatefulWidget {
+  final DataManager dataManager;
+  const OrderPage({super.key, required this.dataManager});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  @override
+  Widget build(BuildContext context) {
+    if (widget.dataManager.cart.isEmpty) {
+      return const Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text("Your order is empty"),
+      );
+    } else {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            ListView.builder(
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                itemCount: widget.dataManager.cart.length,
+                itemBuilder: (context, index) {
+                  var item = widget.dataManager.cart[index];
+                  return OrderItem(
+                      item: item,
+                      onRemove: (product) {
+                        setState(() {
+                          widget.dataManager.cartRemove(product);
+                        });
+                      });
+                }),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                  "Total: \$${widget.dataManager.cartTotal().toStringAsFixed(2)}"),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 28.0),
+              child: SizedBox(
+                width: 200,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green.shade300),
+                    onPressed: () {
+                      //TODO:
+                    },
+                    child: const Text("Send Order")),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+  }
+}
+
+class OrderItem extends StatelessWidget {
+  final ItemInCart item;
+  final Function onRemove;
+  const OrderItem({super.key, required this.item, required this.onRemove});
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Orders');
+    return Card(
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text("${item.quantity}x"),
+                )),
+            Expanded(
+                flex: 6,
+                child: Text(
+                  item.product.name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                )),
+            Expanded(
+                flex: 2,
+                child: Text(
+                    "\$${(item.product.price * item.quantity).toStringAsFixed(2)}")),
+            Expanded(
+                flex: 1,
+                child: IconButton(
+                    color: Theme.of(context).primaryColor,
+                    onPressed: () {
+                      onRemove(item.product);
+                    },
+                    icon: const Icon(Icons.delete)))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OrderAlert extends StatelessWidget {
+  const OrderAlert({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Your Order"),
+      content: const Text("Your order is being prepared. Thanks!"),
+      actions: [
+        TextButton(
+            onPressed: () => Navigator.pop(context, 'OK'),
+            child: const Text('OK'))
+      ],
+    );
   }
 }

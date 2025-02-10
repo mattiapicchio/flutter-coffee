29a58c005dab8949613b2b96d88ff2e40700a876
Mattia Picchio <picchiomattia@gmail.com>
2025-02-10 13:39:01 +0100

    feat: refactor MenuPage to use DataManager for dynamic menu fetching and improve product display

--- lib/pages/menupage.dart
+++ lib/pages/menupage.dart
@@ -1,34 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_coffee/datamodel.dart';
+import 'package:flutter_coffee/datamanager.dart';
 
 class MenuPage extends StatelessWidget {
-  const MenuPage({super.key});
+  final DataManager dataManager;
+
+  const MenuPage({super.key, required this.dataManager});
 
   @override
   Widget build(BuildContext context) {
-    var product = Product(
-      id: 1,
-      name: 'Cappuccino',
-      price: 3.5,
-      image: 'cappuccino.jpg',
-    );
-    return ListView(
-      children: [
-        ProductItem(
-          product: product,
-          onAdd: () {},
-        ),
-        ProductItem(product: product,onAdd: () {}),
-        ProductItem(product: product,onAdd: () {}),
-        ProductItem(product: product,onAdd: () {}),
-      ],
+    var screenSize = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: FutureBuilder<List<Category>>(
+        future: dataManager.getMenu(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            var categories = snapshot.data!;
+
+            return ListView.builder(
+                itemCount: categories.length,
+                itemBuilder: (context, index) {
+                  // EACH CATEGORY STARTS HERE
+                  var category = snapshot.data![index];
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 32.0, bottom: 8.0, left: 8.0),
+                        child: Text(
+                          categories[index].name,
+                          style: TextStyle(color: Colors.brown.shade400),
+                        ),
+                      ),
+                      if (screenSize.width < 500)
+                        // EACH MENU ITEM, Mobile Viewport
+                        ListView.builder(
+                          shrinkWrap: true,
+                          physics: ClampingScrollPhysics(),
+                          itemCount: categories[index].products.length,
+                          itemBuilder: (context, prodIndex) {
+                            var product = categories[index].products[prodIndex];
+                            return ProductItem(
+                                product: product,
+                                onAdd: (product) {
+                                  dataManager.cartAdd(product);
+                                });
+                          },
+                        )
+                      else
+                        // EACH MENU ITEM, Large Viewport
+                        Center(
+                          child: Wrap(
+                            alignment: WrapAlignment.spaceAround,
+                            children: [
+                              for (var product in category.products)
+                                SizedBox(
+                                  width: 350,
+                                  child: ProductItem(
+                                    product: product,
+                                    onAdd: (product) =>
+                                        dataManager.cartAdd(product),
+                                  ),
+                                )
+                            ],
+                          ),
+                        )
+                    ],
+                  );
+                });
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+          // By default, show a loading spinner.
+          return const CircularProgressIndicator();
+        },
+      ),
     );
   }
 }
 
 class ProductItem extends StatelessWidget {
   final Product product;
-  final Function onAdd;
+  final Function(Product) onAdd;
 
   const ProductItem({
     super.key,
@@ -49,8 +105,8 @@ class ProductItem extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Image.asset(
-                "images/black_coffee.png",
+              Image.network(
+                product.imageUrl,
                 width: double.infinity,
                 fit: BoxFit.cover,
               ),
@@ -81,8 +137,10 @@ class ProductItem extends StatelessWidget {
                   Padding(
                     padding: const EdgeInsets.only(right: 8.0),
                     child: ElevatedButton(
-                        onPressed: () => onAdd(product),
-                        child: Text('Add to Cart')),
+                        onPressed: () {
+                          onAdd(product);
+                        },
+                        child: Text('Add')),
                   )
                 ],
               )
